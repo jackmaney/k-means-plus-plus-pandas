d04f3059059080e6af6d4e14147e8ee56dfcbc41
Jack Maney <jackmaney@gmail.com>
2014-01-10 17:28:30 -0600

    Renamed to conform with module name spec, and did some debugging

--- k_means_plus_plus.py
+++ k_means_plus_plus.py
@@ -33,20 +33,20 @@ class KMeansPlusPlus:
 			for col in columns:
 				if col not in data_frame.columns:
 					raise Exception("Column '%s' not found in the given DataFrame" % col)
-				if not self.__is_numeric(col):
+				if not self._is_numeric(col):
 					raise Exception("The column '%s' is either not numeric or contains NaN values" % col)
 			self.columns = columns
 
-	def __populate_initial_centers(self):
+	def _populate_initial_centers(self):
 		rows = []
-		rows.append(self.__grab_random_point())
+		rows.append(self._grab_random_point())
 		distances = None
 
 		while len(rows) < self.k:
 			if distances is None:
-				distances = self.__distances_from_point(rows[0])
+				distances = self._distances_from_point(rows[0])
 			else:
-				distances = self.__distances_from_point_list(rows)
+				distances = self._distances_from_point_list(rows)
 
 			normalized_distances = distances/distances.sum()
 			normalized_distances.sort()
@@ -55,20 +55,20 @@ class KMeansPlusPlus:
 			index = normalized_distances[normalized_distances == min_over_roll].index[0]
 			rows.append(self.data_frame[self.columns].iloc[index,:])
 
-		self.centers = DataFrame(rows)
+		self.centers = DataFrame(rows,columns=self.columns)
 
-	def __compute_distances(self):
+	def _compute_distances(self):
 		if self.centers is None:
 			raise Exception("Must populate centers before distances can be calculated!")
 
 		column_dict = {}
 
 		for i in list(range(self.k)):
-			column_dict[i] = self.__distances_from_point(self.centers.iloc[i,:])
+			column_dict[i] = self._distances_from_point(self.centers.iloc[i,:])
 
 		self.distance_matrix = DataFrame(column_dict,columns=list(range(self.k)))
 
-	def __get_clusters(self):
+	def _get_clusters(self):
 		if self.distance_matrix is None:
 			raise Exception("Must compute distances before closest centers can be calculated")
 
@@ -77,8 +77,8 @@ class KMeansPlusPlus:
 		cluster_list = [
 			boolean_series.index[j][0]
 				for boolean_series in [
-					self.distance_matrix.iloc[1,:] == min_distances[i]
-						for i in list(range(self.numRows))
+					self.distance_matrix.iloc[i,:] == min_distances[i]
+						for i in list(range(len(self.columns)))
 				]
 				for j in list(range(self.k))
 			if boolean_series[j]
@@ -86,7 +86,7 @@ class KMeansPlusPlus:
 
 		self.clusters = Series(cluster_list)
 
-	def __compute_new_centers(self):
+	def _compute_new_centers(self):
 		if self.centers is None:
 			raise Exception("Centers not initialized!")
 
@@ -98,9 +98,9 @@ class KMeansPlusPlus:
 
 	def cluster(self):
 
-		self.__populate_initial_centers()
-		self.__compute_distances()
-		self.__get_clusters()
+		self._populate_initial_centers()
+		self._compute_distances()
+		self._get_clusters()
 
 		counter = 0
 
@@ -109,54 +109,33 @@ class KMeansPlusPlus:
 
 			self.previous_clusters = self.clusters.copy()
 
-			self.__compute_new_centers()
-			self.__compute_distances()
-			self.__get_clusters()
+			self._compute_new_centers()
+			self._compute_distances()
+			self._get_clusters()
 
 			if max_iterations is not None and counter >= max_iterations:
 				break
 			elif all(self.clusters == self.previous_clusters):
 				break
 
+	def _distances_from_point(self,point):
+		return np.power(self.data_frame[self.columns] - point,2).sum(axis=1) #pandas Series
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	def __distances_from_point(self,point):
-		return np.power(self.data_frame[columns] - point,2).sum(axis=1) #pandas Series
-
-	def __distances_from_point_list(self,point_list):
+	def _distances_from_point_list(self,point_list):
 		result = None
 
 		for point in point_list:
 			if result is None:
-				result = self.__distances_from_point(point)
+				result = self._distances_from_point(point)
 			else:
-				result = pd.concat([result,self.__distances_from_point(point)],axis=1).min(axis=1)
+				result = pd.concat([result,self._distances_from_point(point)],axis=1).min(axis=1)
 
 		return result
 
 
-
-
-	def __grab_random_point(self):
+	def _grab_random_point(self):
 		index = np.random.random_integers(0,self.numRows - 1)
 		return self.data_frame[self.columns].iloc[index,:].values #NumPy array
 
-
-
-
-	def __is_numeric(self,col):
+	def _is_numeric(self,col):
 		return all(np.isreal(self.data_frame[col])) and not any(np.isnan(self.data_frame[col]))
