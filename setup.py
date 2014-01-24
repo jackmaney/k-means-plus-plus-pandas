56b652cf013d97ef21e991a3dd3634555f18bdce
Jack Maney <jackmaney@gmail.com>
2014-01-23 20:16:34 -0600

    Trying to make PyPI play nice...

--- setup.py
+++ setup.py
@@ -18,6 +18,8 @@ setup(
     long_description=open("README.md").read(),
     license="MIT",
     url="https://github.com/jackmaney/k-means-plus-plus-pandas",
+    download_url="https://github.com/jackmaney/k-means-plus-plus/releases/tag/v" +
+        kmpp.__version__,
     packages=["clustering"],
     install_requires=requirements
 )
